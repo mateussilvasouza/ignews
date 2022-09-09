81bf99da221de3f8f5431a3b665652622153d851
mateussilvasouza <mathheussilvasouza@gmail.com>
2022-09-09 15:49:03 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+      <title>Início | Ig.News</title>
+      </Head>
+      <h1>Hello <span>World!</span></h1>
+    </>
+  )
+}
