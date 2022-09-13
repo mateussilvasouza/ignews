b5dd21f5d0b2bf8209aa83d742c1290d868b9bf1
mateussilvasouza <mathheussilvasouza@gmail.com>
2022-09-13 18:49:10 -0300

    Creating layout

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,9 +1,26 @@
-import { FaGithub } from 'react-icon'
+import { useState } from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
 
 export function SignInButton(){
-    return(
-        <button type="button">
-            <FaGithub />
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(true) 
+    
+    return isUserLoggedIn ? (
+        <button 
+            className={styles.signInButton}
+            type="button"
+        >
+            <FaGithub color='#04d361' />
+            Mateus Silva Souza
+            <FiX color='#737380' className={styles.closeIcon}/>
+        </button>
+    ) : ( 
+        <button 
+        className={styles.signInButton}
+        type="button"
+        >
+            <FaGithub color='#eba417' />
             Sign in with Github
         </button>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from './home.module.scss'
+
 
 export default function Home() {
   return (
     <>
       <Head>
-      <title>Início | Ig.News</title>
+        <title>Início | Ig.News</title>
       </Head>
-      <h1>Hello <span>World!</span></h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br/> the <span>React</span> world.</h1>
+          <p>
+            Get accesss to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+          <SubscribeButton/>
+        </section> 
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
