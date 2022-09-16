34da586777a654bae7cab92af75b192e0335b851
mateussilvasouza <mathheussilvasouza@gmail.com>
2022-09-16 14:47:01 -0300

    FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,30 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
 
 export const authOptions = {
   // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
+      clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user}) {
+      const {email} = user
+      await fauna.query(
+        q.Create(
+          q.Collection('user'),
+          {data:{email}}
+        )
+      )
+      return true
+    },
+  }
 }
 
 export default NextAuth(authOptions)
